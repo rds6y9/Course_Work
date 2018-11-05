d4ea80204d4612807b62b2f8b6e0dce371c4f238
Ryan Stoughton <ryandstoughton@gmail.com>
2018-11-04 23:32:18 -0600

    Finished hw6

--- CS5204_DataMining/hw6/hw6.py
+++ CS5204_DataMining/hw6/hw6.py
@@ -12,27 +12,97 @@ data = Dataset.load_from_file(file_path, reader=reader)
 # Split data once to ensure same fold is compared across the board.
 data.split(n_folds=3) 
 
-# Part 5
-algo = SVD()
-perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
-print_perf(perf)
+# # Part 5
+# algo = SVD()
+# perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
+# print_perf(perf)
 
-# Part 6
-algo = SVD(biased=False)
-perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
-print_perf(perf)
+# # Part 6
+# algo = SVD(biased=False)
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
 
-# Part 7
-algo = NMF()
-perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
-print_perf(perf)
+# # Part 7
+# algo = NMF()
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
 
-# Part 8 
-algo = KNNBasic(sim_options = {'user_based': True })
-perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
-print_perf(perf)
+# # Part 8 
+# algo = KNNBasic(sim_options = {'user_based': True })
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
 
-# Part 9
-algo = KNNBasic(sim_options = {'user_based': False })
-perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
-print_perf(perf)
+# # Part 9
+# algo = KNNBasic(sim_options = {'user_based': False })
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
+
+# Part 14
+## User-Based Flavors
+# print('User-Based MSD')
+# algo = KNNBasic(sim_options = {
+#     'name': 'MSD', 
+#     'user_based': True 
+# })
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
+
+# print('User-Based Cosine')
+# algo = KNNBasic(sim_options = {
+#     'name': 'cosine', 
+#     'user_based': True 
+# })
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
+
+# print('User-Based Pearson')
+# algo = KNNBasic(sim_options = {
+#     'name': 'pearson', 
+#     'user_based': True 
+# })
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
+
+# ## Item-Based Flavors
+# print('Item-Based MSD')
+# algo = KNNBasic(sim_options = {
+#     'name': 'MSD', 
+#     'user_based': False
+# })
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
+
+# print('Item-based Cosine')
+# algo = KNNBasic(sim_options = {
+#     'name': 'cosine', 
+#     'user_based': False
+# })
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
+
+# print('Item-based Pearson')
+# algo = KNNBasic(sim_options = {
+#     'name': 'pearson', 
+#     'user_based': False
+# })
+# perf = evaluate(algo, data,measures=['RMSE', 'MAE'])
+# print_perf(perf)
+
+# Part 15
+for i in range(1, 30):
+    print("kNN: " + str(i) + " for User-Based collaborative filtering.")
+    algo = KNNBasic(sim_options = {
+        'name': 'MSD', 
+        'user_based': True 
+    })
+    perf = evaluate(algo, data,measures=['RMSE'])
+    print_perf(perf)
+
+for i in range(1, 30):
+    print("kNN: " + str(i) + " for Item-Based collaborative filtering.")
+    algo = KNNBasic(sim_options = {
+        'name': 'MSD', 
+        'user_based': False
+    })
+    perf = evaluate(algo, data,measures=['RMSE'])
+    print_perf(perf)
